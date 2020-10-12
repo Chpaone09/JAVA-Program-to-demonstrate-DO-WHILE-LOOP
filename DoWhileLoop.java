ec2c0db2199b580b3401c1ae4c6e4ef73eaa455c
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:58:37 +0530

    Program to demonstrate DO-WHILE LOOP in java

---
+++ DoWhileLoop.java
@@ -0,0 +1,21 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate DO-WHILE LOOP in java.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+public class DoWhileLoop{
+    public static void main(String[] args) {
+        int i =1;
+        System.out.print("\n\n\t Demonstration of DO-WHILE LOOP in JAVA >> ");
+        do{
+            System.out.print(" "+(i++));
+        }while(i<=5);
+    }
+    }
+    
+    
